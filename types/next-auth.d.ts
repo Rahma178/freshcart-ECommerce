c391c0a9b2d9693e389f3f494eef0d2eb1778a25
Rahma178 <sarhanrahma83@gmail.com>
2026-04-13 20:16:26 +0200

    first commit

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -9,9 +9,6 @@ interface ApplicationUser {
 }
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
 
   interface User {
     name: string,
