694dd070c90c7f18883a1087d789d81d4f85b5a3
Rahma178 <sarhanrahma83@gmail.com>
2026-04-13 20:39:24 +0200

    second commit

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,15 +1,28 @@
-
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function proxy(req: NextRequest) {
-  const token = await getToken({ req });
-  console.log("token");
-  if (!!token) return NextResponse.next();
+export async function middleware(req: NextRequest) {
+  const cookieName =
+    process.env.NODE_ENV === "production"
+      ? "__Secure-next-auth.session-token"
+      : "next-auth.session-token";
 
-  return NextResponse.redirect(new URL('/login', req.url));
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    cookieName,
+  });
+
+  if (token) return NextResponse.next();
+
+  return NextResponse.redirect(new URL("/login", req.url));
 }
 
 export const config = {
-  matcher: ["/cart"],
+  matcher: [
+    "/cart/:path*",
+    "/whishList/:path*",
+    "/allorders/:path*",
+    "/CheckOut/:path*",
+  ],
 };
--- src/utilities/getTokenFn.ts
+++ src/utilities/getTokenFn.ts
@@ -1,17 +1,17 @@
-import { decode } from "next-auth/jwt"
-import { cookies } from "next/headers"
+import { decode } from "next-auth/jwt";
+import { cookies } from "next/headers";
 
-
-export async function getToken():Promise<string>{
-    const cookie = await cookies()
-  const nextAuthToken = cookie.get("next-auth.session-token")?.value
+export async function getToken(): Promise<string> {
+  const cookie = await cookies();
+  
+  const nextAuthToken =
+    cookie.get("next-auth.session-token")?.value ||
+    cookie.get("__Secure-next-auth.session-token")?.value;
 
   const decodeCookie = await decode({
     secret: process.env.NEXTAUTH_SECRET!,
-    token: nextAuthToken
-  })
+    token: nextAuthToken,
+  });
 
-  console.log(decodeCookie)
-
-  return decodeCookie?.token as string
+  return decodeCookie?.token as string;
 }
